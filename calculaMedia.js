918d5321fb676cedfb08dc3fc28384ba6b93e449
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2026-02-09 17:30:27 +0000

    finalizando

--- calculaMedia.js
+++ calculaMedia.js
@@ -1,14 +1,22 @@
-console.log("Digite sua nota");
 
-let nota1 = 4;
-let nota2 = 5;
-let nota3 = 9
-let nota4 = 5
+let nota1 = 10;
+let nota2 = 10;
+let nota3 = 10;
+let nota4 = 10;
 
 const nomeAluno = "Cauan";
 
 let media = (nota1 + nota2 + nota3 + nota4) / 4
 
+if (media >7){
+    console.log("O aluno " + nomeAluno + " está aprovado")
+}
+if(media >= 5 && media < 7){
+    console.log("O aluno " + nomeAluno + " está no se liga")    
+}
+if(media <= 5){
+     console.log("O aluno " + nomeAluno + " está reprovado")
+}
 
 console.log("O aluno: " + nomeAluno);
 console.log("Tem a média: " + media);
