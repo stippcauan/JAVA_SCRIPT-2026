51be5e0516c39886ccb56fd99508c6132bace27a
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2026-02-09 13:05:46 +0000

    começado

---
+++ calculaMedia.js
@@ -0,0 +1,14 @@
+console.log("Digite sua nota");
+
+let nota1 = 4;
+let nota2 = 5;
+let nota3 = 9
+let nota4 = 5
+
+const nomeAluno = "Cauan";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+
+console.log("O aluno: " + nomeAluno);
+console.log("Tem a média: " + media);
