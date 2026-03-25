2c60010b6ac9c11458fe779fc0dc7af87c748fc6
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2026-03-25 13:03:38 +0000

    vetor

---
+++ vetor.js
@@ -0,0 +1,19 @@
+let compras = ["Morango", "Banana", "Pão", "Leite", "Queijo"];
+
+//onsole.log(compras)
+
+//console.log("O primeiro item da lista é " + compras[0]);
+//console.log("O último item da lista é " + compras[4]);
+
+compras[2] = "macarrão";
+//console.log(compras[2]);
+compras.push("Coca-cola")
+compras.push("Limão");
+//console.log(compras);
+compras.pop()
+compras.pop()
+compras.pop()
+
+console.log(compras);
+
+console.log(compras.length);
